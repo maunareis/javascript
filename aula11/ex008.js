6eba4dbf50a3f74fad2184431cb95e7ac98c0458
maunareis <110308633+maunareis@users.noreply.github.com>
2022-08-21 19:23:10 -0300

    Aprendendo Condições if/else

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70.5
+console.log(`A velocidade de seu carro é ${vel} Km/h`)
+if ( vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
