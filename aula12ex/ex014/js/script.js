185a3f1261ca676db822c0efdd6c60d6fe6e1b1f
maunareis <110308633+maunareis@users.noreply.github.com>
2022-08-27 14:58:23 -0300

    Exercício 1 aula 12

--- aula12ex/ex014/js/script.js
+++ aula12ex/ex014/js/script.js
@@ -1,1 +1,26 @@
-alert('Ola')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var agora = new Date()
+    var hora = agora.getHours()
+    //var hora = 22
+    var min = agora.getMinutes()
+    msg.innerHTML = `Agora são ${hora} horas e ${min} minutos.`
+    if (hora < 6){
+        //madrugada
+        img.src = 'imagens/madrugadaed.png'
+        document.body.style.background = '#3851ae'
+    } else if (hora < 12){
+        //Bom dia
+        img.src = 'imagens/manhaed.png'
+        document.body.style.background = '#c08d64'
+    } else if (hora < 18){
+        //Boa tarde
+        img.src = 'imagens/tardeed.png'
+        document.body.style.background = '#c7410f'
+    } else {
+        //Boa noite.
+        img.src = 'imagens/noiteed.png'
+        document.body.style.background = '#60486c'
+    }
+}
