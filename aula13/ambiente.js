dda4f06ab567170d7c7496d415fb741888969eb3
maunareis <110308633+maunareis@users.noreply.github.com>
2022-09-01 21:06:43 -0300

    Aprendendo a usar repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while(c<=400)
+    
+
+    
+    
+ /*
+ var c = 1
+while(c<=500){
+    console.log('Tudo bem?')
+    c++
+}*/    
+    
+ /*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
