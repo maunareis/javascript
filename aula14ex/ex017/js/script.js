bc12f97a5d76e315e7bc4f71d697bb7d6d38204b
maunareis <110308633+maunareis@users.noreply.github.com>
2022-09-07 09:26:26 -0300

    Criado ex 017

--- aula14ex/ex017/js/script.js
+++ aula14ex/ex017/js/script.js
@@ -1,1 +1,24 @@
-alert('Ola')
+function gerar(){
+    var n = document.querySelector('input#num')
+
+    if (n.value.length == 0){
+        window.alert(`Por favor, digite um número!`)
+    } else {
+            while (c<=10){
+                var mult = n.value 
+                var res = mult * c
+                res.innerHTML = `${res}`
+            }
+    }
+
+       // res.innerHTML = n.value
+    }
+
+
+/*
+for(var c = 1; c = 10; c++){
+                var res = n.value * c
+                res.innerHTML = res
+                */
+
+
