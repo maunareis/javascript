108951a3c02057b96443144c3a354e2c09d26216
maunareis <110308633+maunareis@users.noreply.github.com>
2022-09-21 21:08:24 -0300

    Exercício aula 16

--- aula16ex/ex01/js/script.js
+++ aula16ex/ex01/js/script.js
@@ -20,9 +20,15 @@ function inList(n, l){
 }
 
 function adicionar(){
-    if(isNumero(fnum.value) && !inList(fnum.value, valores)){
-        window.alert('Tudo ok!')
+    if(isNumero(fnum.value) && !inList(fnum.value, valores)){//se for realmente um número e se esse número não está na lista
+        valores.push(Number(fnum.value)) // adiciona no vetor o número digitado
+        let item = document.createElement('option') // cria o elemento option
+        item.text = `Valor ${fnum.value} adicionado.` // escreve dentro do ultimo item adicionado
+        flist.appendChild(item) //cria um elemento filho
+
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
+    fnum.value = `` // para apagar automaticamente valor digitado
+    fnum.focus() // faz com que o cursor volte para o local de digitação
 }
